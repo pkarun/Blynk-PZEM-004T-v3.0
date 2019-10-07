f7eccbdc043c818588d1bf744870149d9ecc8132
Arun desktop <pkarun29@gmail.com>
2019-10-07 20:37:44 +0530

    changed file structure

--- Blynk-PZEM-004T-v3.0/settings.h
+++ Blynk-PZEM-004T-v3.0/settings.h
@@ -16,31 +16,8 @@
 #define vPIN_POWER_FACTOR           V5
 #define vPIN_OVER_POWER_ALARM       V6
 
-
-/***************************************************
- *       Blynk Settings 
- **************************************************/
- 
-#define AUTH "Your Blynk Auth Token" //You should get Auth Token in the Blynk App. 
-
-
-/***************************************************
- *        WiFi Settings
- **************************************************/
-  
-#define WIFI_SSID "Your WiFi Network Name"
-#define WIFI_PASS "Your Wifi Password"
-
 /***************************************************
  *        Server Settings
  **************************************************/
       
 #define OTA_HOSTNAME "PZEM-004v3"
-
-
-// Remove/comment-out below 2 lines when use Blynk hosted server
- 
-#define USE_LOCAL_SERVER       //Use local Blynk Server - comment-out if use Blynk hosted cloud service
-#define SERVER "xxx.xxx.xx.xxx" //comment-out if use Blynk hosted cloud service
-
-#define PORT 8080
